91ae5829c64227b5f9e769974e280d2813cbbfea
Anderson Alves <anderson.pereira@basis.com.br>
2018-04-23 00:52:36 -0300

    Ajustando botoes links de usuarios

--- application/controllers/listuser.php
+++ application/controllers/listuser.php
@@ -1,21 +1,20 @@
 <?php
+include  '../../configs/conection.php';
 /**
  * Created by PhpStorm.
  * User: anderson
  * Date: 16/04/18
  * Time: 14:58
  */
-include_once('../../configs/conection.php');
 
-$buscaDados = "select membros.member_id, membros.cpf, membros.membros.tel_cel, tb_status.ds_status, membros.nome, membros.email, tb_acess_level.ds_nome_level     
+$buscaDados = "select membros.member_id, membros.cpf, membros.membros.tel_cel, membros.dt_aniver, tb_status.ds_status, membros.nome, membros.email, tb_acess_level.ds_nome_level     
 from membros.tb_status 
 inner join membros.membros on (tb_status.id_status = membros.membros.status) 
 inner join membros.tb_acess_level on (tb_acess_level.id_level = membros.level_acess)";
 
 $sql1 = pg_query($buscaDados);
-$date = pg_fetch_assoc($sql1);
 
-//echo "<pre>"; print_r(pg_fetch_assoc($sql1));die; echo "</pre>";
+
 
 
 
